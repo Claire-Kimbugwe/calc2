daaddd8cde797e683d597630b2c8f4550bf31412
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-09 15:45:35 -0800

    Calculator 2

---
+++ calculator.py
@@ -0,0 +1,15 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+user_input = input("Please enter your operand and numbers. ")
+user_input = user_input.split()
+symbol = user_input[0]
+print(user_input)
+print(symbol)
+
+
+# Your code goes here
